9d7ac388e35ce37e54a9b820657dc1ab1691905b
Zarar Siddiqi <zarar_siddiqi@epam.com>
2018-04-11 11:56:49 -0400

    Dynamic port allocation.

--- morningcoffee.py
+++ morningcoffee.py
@@ -113,6 +113,7 @@ def home():
 
 
 if __name__ == "__main__":
-        morningcoffee.run(debug=False, host='0.0.0.0')
+        port = int(os.environ.get('PORT', 5000))
+        morningcoffee.run(debug=False, host='0.0.0.0', port=port)
 
 
